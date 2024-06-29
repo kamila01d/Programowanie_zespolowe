d3ff3d975a56566c3738008601737f2f19a08507
ChristianCitterio <christiancitterio18@gmail.com>
2024-06-29 19:51:23 +0200

    fix(API): adds root_path

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
 
 from src.api.routes.auth_routes import auth_router
 from src.api.routes.products_routes import products_router
 from src.api.routes.users_routes import users_router
 
-app = FastAPI()
+app = FastAPI(root_path="/p1")
 app.include_router(auth_router)
 app.include_router(users_router)
 app.include_router(products_router)
+
+app.mount("/", StaticFiles(directory="./Programowanie_zespolowe/src/frontend", html=True), name="frontend")
 
 
 @app.get("/healtz")

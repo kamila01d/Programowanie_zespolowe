b344c8a9983dec4f654a3e0d299f94beb9a874ff
ChristianCitterio <christiancitterio18@gmail.com>
2024-06-29 23:03:43 +0200

    feat(API): adds cors

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.staticfiles import StaticFiles
 
 from src.api.routes.auth_routes import auth_router
@@ -11,6 +12,14 @@ app.include_router(auth_router)
 app.include_router(users_router)
 app.include_router(products_router)
 app.include_router(compare_product_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount(
     "/",
